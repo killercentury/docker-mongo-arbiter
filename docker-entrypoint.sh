9b2ff7757e34327299c9a176981d65dd7c6e222f
Kira Cheung <killercentury@gmail.com>
2015-03-23 19:56:07 +1100

    add dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- mongod "$@"
+fi
+
+if [ "$1" = 'mongod' ]; then
+	chown -R mongodb /data/arb
+
+	numa='numactl --interleave=all'
+	if $numa true &> /dev/null; then
+		set -- $numa "$@"
+	fi
+
+	exec gosu mongodb "$@"
+fi
+
+exec "$@"
